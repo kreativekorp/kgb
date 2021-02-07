b76e2cede48fe9a6b1d36cb3f8ffdfc4626c62f4
Rebecca G. Bettencourt <beckiejo@gmail.com>
2021-02-06 22:34:18 -0800

    Moved to https://github.com/kreativekorp/recode

--- java/KreativeGoodieBag/src/com/kreative/recode/transformation/TextTransformationGUI.java
+++
@@ -1,8 +0,0 @@
-package com.kreative.recode.transformation;
-
-import java.awt.Component;
-
-public interface TextTransformationGUI {
-	public Component getComponent();
-	public TextTransformation createTransformation();
-}
